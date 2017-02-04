f689821554a678266872b161d5be574374cb335a
Serge Lobatch <serge@lobat.ch>
2017-02-03 20:41:29 -0500

    New sheet for dark styles & functionalize JS
    Instead of manually changing individual styles in the javascript, just
    change the stylesheet.

--- js/getdark.js
+++ js/getdark.js
@@ -1,31 +1,115 @@
+// Variable Definition.
+var defaultSheet = "/darkout/css/main.css";
+var lightSheet = "/darkout/css/main.css";
+var darkSheet = "/darkout/css/dark-main.css";
+var lightButtonString = "It's light out! <span class='emoji'>😎</span>";
+var darkButtonString = "It's dark out! <span class='emoji'>😴</span>";
+var loadButtonString = "Getting your location... <i class='fa fa-spinner fa-spin' aria-hidden='true'></i>";
+var errorString = "<i class='fa fa-exclamation-triangle' aria-hidden='true'></i> Please approve location permissions...";
+var unsupportedButtonString = "<i class='fa fa-exclamation-triangle' aria-hidden='true'></i> Browser doesn't support location... ";
+
+
 var date = null;
 var a = null;
 var aText = null;
+
 var locationOptions = {
   enableHighAccuracy: true,
   timeout: 5000,
   maximumAge: 0
 };
 
+
+// Once the window loads...
 window.onload = function() {
 
+  // Variable Assignment
   a = document.getElementById("start-button");
   aText = document.getElementsByClassName("button-text")[0];
+
+  // Button click behavior
   a.onclick = function() {
-    getDark();
+      getDark();
+    };
+  };
+
+
+// Get user's position & callback a function that acts on the time of day.
+
+function getDark(){
+
+  date = new Date();
+
+  if (navigator.geolocation) {
+
+    updateButton("warning", loadButtonString);
+
+    navigator.geolocation.getCurrentPosition(isDark, error, locationOptions);
+
+  } else {
+
+    console.log("Geolocation is not supported by this browser.");
+    updateButton("danger", unsupportedButtonString);
+
+    return false;
+
   }
 }
 
-// Get Position
-function error(err){
-  console.warn(`ERROR(${err.code}): ${err.message}`);
 
-  a.className = "btn btn-lg btn-danger";
+// Calculate darkness from user's position. [Called back by getDark()]
+function isDark(position){
 
-  transitionText("<i class='fa fa-exclamation-triangle' aria-hidden='true'></i> Please approve location permissions...");
+  //console.log(position);
+
+  var times = SunCalc.getTimes(date, position.coords.latitude, position.coords.longitude);
+  var dusk = times.dusk;
+  var dawn = times.dawn;
+
+  if (date > dawn && date < dusk){
+
+    changeStylesheet(lightSheet);
+    updateButton("success", lightButtonString);
+
+  } else {
+
+    changeStylesheet(darkSheet);
+    updateButton("success", darkButtonString);
+
+  }
 }
 
-function transitionText(newText){
+// Handle errors for getDark().
+function error(err){
+
+  console.warn(`ERROR (${err.code}: ${err.message})`);
+  updateButton("danger", errorString);
+
+}
+
+
+// Update button text & style
+function updateButton(newStyle, newText){
+
+  // Set button style
+  switch (newStyle) {
+    case "primary":
+      a.className = "btn btn-lg btn-primary";
+      break;
+    case "warning":
+      a.className = "btn btn-lg btn-warning";
+      break;
+    case "danger":
+      a.className = "btn btn-lg btn-danger";
+      break;
+    case "success":
+      a.className = "btn btn-lg btn-success";
+      break;
+    default:
+      a.className = "btn btn-lg";
+  }
+
+  // Update button text with smooth transition.
 
   aText.style.opacity = 0;
 
@@ -35,49 +119,8 @@ function transitionText(newText){
   }, 500);
 }
 
-function getDark(){
+// Change Stylesheet
 
-  date = new Date();
-
-  if (navigator.geolocation) {
-
-    a.className = "btn btn-lg btn-warning";
-
-    document.body.style.backgroundColor = "";
-    document.body.style.color = "";
-
-    transitionText("Getting your location... <i class='fa fa-spinner fa-spin' aria-hidden='true'></i>");
-
-    navigator.geolocation.getCurrentPosition(isDark, error, locationOptions);
-  } else {
-    console.log("Geolocation is not supported by this browser.")
-
-    a.className = "btn btn-lg btn-danger";
-
-    transitionText("<i class='fa fa-exclamation-triangle' aria-hidden='true'></i> Browser doesn't support location... ");
-    return false;
-  }
-}
-
-
-
-// Get sunrise & sunset times for current location.
-function isDark(position){
-  console.log(position);
-  var times = SunCalc.getTimes(date, position.coords.latitude, position.coords.longitude)
-  var dusk = times.dusk;
-  var dawn = times.dawn;
-
-  if (date > dawn && date < dusk){
-    document.body.style.backgroundColor = "";
-    document.body.style.color = "";
-    a.className = "btn btn-lg btn-success";
-    transitionText("It's light out! <span class='emoji'>😎</span>");
-  }
-  else {
-    document.body.style.backgroundColor = "#493a8c";
-    document.body.style.color = "#fafafa";
-    a.className = "btn btn-lg btn-success";
-    transitionText("It's dark out! <span class='emoji'>😴</span>");
-  }
+function changeStylesheet(sheet){
+  document.getElementById("pagestyle").setAttribute("href", sheet);
 }
