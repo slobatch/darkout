6d52550f7e22acd713f0b276a79c435c18e5f3cc
Serge Lobatch <serge@lobat.ch>
2017-02-08 20:18:04 -0500

    Display map when location obtained.

--- js/getdark.js
+++ js/getdark.js
@@ -7,11 +7,13 @@ var darkButtonString = "It's dark out! <span class='emoji'>😴</span>";
 var loadButtonString = "Getting your location... <i class='fa fa-spinner fa-spin' aria-hidden='true'></i>";
 var errorString = "<i class='fa fa-exclamation-triangle' aria-hidden='true'></i> Please approve location permissions...";
 var unsupportedButtonString = "<i class='fa fa-exclamation-triangle' aria-hidden='true'></i> Browser doesn't support location... ";
-
+var mapNightStyle = [ {elementType: 'geometry', stylers: [{color: '#242f3e'}]}, {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]}, {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]}, { featureType: 'administrative.locality', elementType: 'labels.text.fill', stylers: [{color: '#d59563'}] }, { featureType: 'poi', elementType: 'labels.text.fill', stylers: [{color: '#d59563'}] }, { featureType: 'poi.park', elementType: 'geometry', stylers: [{color: '#263c3f'}] }, { featureType: 'poi.park', elementType: 'labels.text.fill', stylers: [{color: '#6b9a76'}] }, { featureType: 'road', elementType: 'geometry', stylers: [{color: '#38414e'}] }, { featureType: 'road', elementType: 'geometry.stroke', stylers: [{color: '#212a37'}] }, { featureType: 'road', elementType: 'labels.text.fill', stylers: [{color: '#9ca5b3'}] }, { featureType: 'road.highway', elementType: 'geometry', stylers: [{color: '#746855'}] }, { featureType: 'road.highway', elementType: 'geometry.stroke', stylers: [{color: '#1f2835'}] }, { featureType: 'road.highway', elementType: 'labels.text.fill', stylers: [{color: '#f3d19c'}] }, { featureType: 'transit', elementType: 'geometry', stylers: [{color: '#2f3948'}] }, { featureType: 'transit.station', elementType: 'labels.text.fill', stylers: [{color: '#d59563'}] }, { featureType: 'water', elementType: 'geometry', stylers: [{color: '#17263c'}] }, { featureType: 'water', elementType: 'labels.text.fill', stylers: [{color: '#515c6d'}] }, { featureType: 'water', elementType: 'labels.text.stroke', stylers: [{color: '#17263c'}] } ]
 
 var date = null;
 var a = null;
 var aText = null;
+var map = null;
+var mapDiv = null;
 
 var locationOptions = {
   enableHighAccuracy: true,
@@ -26,6 +28,7 @@ window.onload = function() {
   // Variable Assignment
   a = document.getElementById("start-button");
   aText = document.getElementsByClassName("button-text")[0];
+  mapDiv = document.getElementById("map");
 
   // Button click behavior
   a.onclick = function() {
@@ -33,7 +36,8 @@ window.onload = function() {
       changeStylesheet(defaultSheet);
       getDark();
     };
-  };
+
+};
 
 
 // Get user's position & callback a function that acts on the time of day.
@@ -62,7 +66,8 @@ function getDark(){
 // Calculate darkness from user's position. [Called back by getDark()]
 function isDark(position){
 
-  //console.log(position);
+  // console.log(position);
+  //
 
   var times = SunCalc.getTimes(date, position.coords.latitude, position.coords.longitude);
   var dusk = times.dusk;
@@ -72,11 +77,13 @@ function isDark(position){
 
     changeStylesheet(lightSheet);
     updateButton("success", lightButtonString);
+    buildMap(position, "day");
 
   } else {
 
     changeStylesheet(darkSheet);
     updateButton("success", darkButtonString);
+    buildMap(position, "night");
 
   }
 }
@@ -125,4 +132,45 @@ function updateButton(newStyle, newText){
 
 function changeStylesheet(sheet){
   document.getElementById("pagestyle").setAttribute("href", sheet);
+}
+
+
+// var initMap = function() {
+//   // Create a map object and specify the DOM element for display.
+//   console.log("running map");
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 40.6209980, lng: -74.0241120},
+//     scrollwheel: false,
+//     zoom: 8
+//   });
+// }
+
+function buildMap(position, version){
+  mapDiv.style.height = "200px";
+  mapDiv.style.width = "100%";
+
+  var userPosition = {lat: position.coords.latitude, lng: position.coords.longitude};
+
+  if (version == "night") {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: userPosition,
+      scrollwheel: false,
+      zoom: 10,
+      styles: mapNightStyle
+    });
+  } else {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: userPosition,
+      scrollwheel: false,
+      zoom: 10
+    });
+  }
+
+  var marker = new google.maps.Marker({
+    position: userPosition,
+    map: map,
+    title: 'You are here!'
+  });
+
+  mapDiv.style.display = "block";
 }
